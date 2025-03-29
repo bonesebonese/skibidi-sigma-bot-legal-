b043231d3e8537f27939dea6fcd4a7dab2e41861
bonese <83821391+bonesebonese@users.noreply.github.com>
2025-03-29 11:22:18 -0300

    Update player.js

--- player.js
+++ player.js
@@ -102,7 +102,7 @@ function initializePlayer(client) {
                 iconURL: musicIcons.playerIcon,
                 url: config.SupportServer
             })
-            .setFooter({ text: `Developed by SSRR | Prime Music v1.2`, iconURL: musicIcons.heartIcon })
+            .setFooter({ text: `Ao seu comando, chefe.`, iconURL: musicIcons.playlistIcon })
             .setTimestamp()
             .setDescription(  
                 `- **Title:** [${track.info.title}](${track.info.uri})\n` +
