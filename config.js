832ce82c178844a327f030dac8f4d9995a079776
bonese <83821391+bonesebonese@users.noreply.github.com>
2025-04-12 16:10:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Shadow Garden", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/r93pTtjhcF",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
