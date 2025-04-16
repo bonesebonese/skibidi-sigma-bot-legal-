684b8ddb8bc35c1f7174fa033d7a26f0f992ce4d
bonese <83821391+bonesebonese@users.noreply.github.com>
2025-04-16 12:52:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "glaceyt",
-      password: "glayceyt",
-      host: "astro.pylex.xyz",
-      port:  11408,
+name: "Avinan",
+      password: "avinan",
+      host: "new-york-node-1.vortexcloud.xyz",
+      port:  5008,
       secure: false
     }
   ]
